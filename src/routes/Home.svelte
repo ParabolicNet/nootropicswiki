8a4a7c2c879b41d61f98408a9d1d02cc7b895263
peepo5 <72892531+peepo5@users.noreply.github.com>
2023-05-06 03:24:22 +0100

    Added Logo

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -6,8 +6,12 @@
 <main>
     <div class="gentle-flex">
     <div style="max-width:1400px;padding-left:15px;padding-right:15px;">
-        <h2>The Nootropics Wiki</h2>
-  
+        <div class="gentle-flex">
+            <img src="/images/nw_logo3.png" style="height:115px;" alt="logo">
+        </div>
+        <div style="">
+        
+        </div>
         <p style="max-width:300px;">
         A crude first version of the Nootropics Wiki to get all relevant information into one place. <a href="https://github.com/ParabolicNet/nootropicswiki">Contribute</a><!-- Sponsored by <a href="https://neuroregen.org" target="_blank">neuroregen</a>.-->
         </p>
